294252100551dc3be25afda15cde80de84788aba
Hridoyshom <hshom827@gmail.com>
2022-02-17 20:27:36 +0600

    add-eventlistener

---
+++ money.js
@@ -0,0 +1,11 @@
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const inputeIncome = document.getElementById('input-income');
+    const costFood = document.getElementById('input-food');
+    const costRent = document.getElementById('input-rent');
+    const costClothes = document.getElementById('input-clothes');
+
+    const inputIncomeText = inputeIncome.value;
+    const inputCost = parseFloat(inputIncomeText);
+
+
+})
